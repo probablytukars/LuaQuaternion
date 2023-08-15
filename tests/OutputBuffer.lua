1d7a8067a1b544d88ccdeca1aa6a34f4fbde2671
Fraser Wallace <89352810+probablytukars@users.noreply.github.com>
2023-08-15 20:48:40 +0100

    Fix tests lua files

--- tests/OutputBuffer.lua
+++ tests/OutputBuffer.lua
@@ -2,60 +2,60 @@ local OutputBuffer = {}
 OutputBuffer.__index = OutputBuffer
 
 function OutputBuffer.new()
-	local self = {}
-	
-	self.buffer = ""
-	setmetatable(self, OutputBuffer)
-	
-	return self
+    local self = {}
+    
+    self.buffer = ""
+    setmetatable(self, OutputBuffer)
+    
+    return self
 end
 
 function OutputBuffer:Prepend(...)
-	local args = {...}
-	local n = #args
-	local argString = ""
-	for i, arg in pairs(args) do
-		local str = tostring(arg)
-		argString ..= str
-		if i < n then
-			argString ..= " "
-		end
-	end
-	self.buffer = argString .. self.buffer
+    local args = {...}
+    local n = #args
+    local argString = ""
+    for i, arg in pairs(args) do
+        local str = tostring(arg)
+        argString = argString .. str
+        if i < n then
+            argString = argString .. " "
+        end
+    end
+    self.buffer = argString .. self.buffer
 end
 
 function OutputBuffer:PrependNL(...)
-	local args = {...}
-	table.insert(args, "\n")
-	self:Prepend(unpack(args))
+    local args = {...}
+    table.insert(args, "\n")
+    self:Prepend(table.unpack(args))
 end
 
 function OutputBuffer:Append(...)
-	local args = {...}
-	local n = #args
-	local argString = ""
-	for i, arg in pairs(args) do
-		local str = tostring(arg)
-		argString ..= str
-		if i < n then
-			argString ..= " "
-		end
-	end
-	self.buffer ..= argString
+    local args = {...}
+    local n = #args
+    local argString = ""
+    for i, arg in pairs(args) do
+        local str = tostring(arg)
+        argString = argString .. str
+        if i < n then
+            argString = argString .. " "
+        end
+    end
+    self.buffer = self.buffer .. argString
 end
 
 function OutputBuffer:AppendNL(...)
-	self:Append(...)
-	self.buffer ..= "\n"
+    self:Append(...)
+    self.buffer = self.buffer .. "\n"
 end
 
 function OutputBuffer:Reset()
-	self.buffer = ""
+    self.buffer = ""
 end
 
 function OutputBuffer:Flush()
-	print(self.buffer)
-	self:Reset()
+    print(self.buffer)
+    self:Reset()
 end
 
 return OutputBuffer
--- tests/Tester.lua
+++ tests/Tester.lua
@@ -1,5 +1,4 @@
-local replicatedStorage = game:GetService("ReplicatedStorage")
-local OutputBuffer = require(replicatedStorage.OuputBuffer)
+local OutputBuffer = require("OutputBuffer")
 
 local buffer = OutputBuffer.new()
 
@@ -7,26 +6,25 @@ local Tester = {}
 Tester.__index = Tester
 
 function Tester.new(testModule, testWithoutProtection)
-	local self = {}
-
+    local self = {}
+    
     self.testModule = testModule
     self.testWithoutProtection = testWithoutProtection or false
-	
-	setmetatable(self, Tester)
-	table.freeze(self)
-	
-	return self
+    
+    setmetatable(self, Tester)
+    
+    return self
 end
 
 local function getResultString(
-	totalTests, testsPassed, testsFailed, unhandledExceptions
+    totalTests, testsPassed, testsFailed, unhandledExceptions
 )
-	return
-		"[" .. tostring(testsPassed)
-			.. " / " .. tostring(totalTests)
-			.. "] tests passed."
-	.. "\nFailures: " .. tostring(testsFailed)
-	.. "\nUnhandled Exceptions: " .. tostring(unhandledExceptions)
+    return
+        "[" .. tostring(testsPassed)
+            .. " / " .. tostring(totalTests)
+            .. "] tests passed."
+        .. "\nFailures: " .. tostring(testsFailed)
+        .. "\nUnhandled Exceptions: " .. tostring(unhandledExceptions)
 end
 
 function Tester:runTest(test, call)
@@ -37,104 +35,104 @@ function Tester:runTest(test, call)
     else
         success, result, reason = pcall(test.test)
     end
-	if success then
-		if result then
-			return 0, "       [✔] Pass - " .. test.DisplayName
-		else
-			return 1, "       [❌] Fail - " .. test.DisplayName
-		end
-	else
-		return 2, "       ❗❗❗ Unhandled Exception - " .. test.DisplayName
-	end
+    if success then
+        if result then
+            return 0, "       [✔] Pass - " .. test.DisplayName
+        else
+            return 1, "       [❌] Fail - " .. test.DisplayName
+        end
+    else
+        return 2, "       ❗❗❗ Unhandled Exception - " .. test.DisplayName
+    end
 end
 
 function Tester:testFunctionGroup(functionGroup)
-	local totalTests = 0
-	local testsPassed = 0
-	local testsFailed = 0
-	local unhandledExceptions = 0
-	
-	local functionGroupOrder = functionGroup._order
-	for _, functionIndexName in pairs(functionGroupOrder) do
-		local functionTests = functionGroup[functionIndexName]
-		local order = functionTests._order
-		local nord = #order
-		local allPassed = true
-		
-		for i, testIndex in pairs(order) do
-			totalTests += 1
-			local test = functionTests[testIndex]
-			local status, result = self:runTest(test)
-			if status == 0 then
-				testsPassed += 1
-			elseif status == 1 then
-				testsFailed += 1
-				allPassed = false
-			else
-				unhandledExceptions += 1
-				allPassed = false
-			end
-			if i < nord then
-				buffer:AppendNL(result)
-			else
-				buffer:Append(result)
-			end
-		end
-		
-		local functionTestsOutput
-		if allPassed then
-			functionTestsOutput = "[✔] All tests passed - "
-				.. functionIndexName
-		else
-			functionTestsOutput = "[❌] Tests failed - "
-				.. functionIndexName
-		end
-		
-		buffer:PrependNL(functionTestsOutput)
-		buffer:Flush()
-	end
-	
-	local resultString = getResultString(
-		totalTests, testsPassed, testsFailed, unhandledExceptions
-	)
-	
-	buffer:AppendNL()
-	buffer:AppendNL(functionGroup._DisplayName)
-	buffer:AppendNL(resultString)
-	buffer:Flush()
-	
-	return totalTests, testsPassed, testsFailed, unhandledExceptions
+    local totalTests = 0
+    local testsPassed = 0
+    local testsFailed = 0
+    local unhandledExceptions = 0
+    
+    local functionGroupOrder = functionGroup._order
+    for _, functionIndexName in pairs(functionGroupOrder) do
+        local functionTests = functionGroup[functionIndexName]
+        local order = functionTests._order
+        local nord = #order
+        local allPassed = true
+        
+        for i, testIndex in pairs(order) do
+            totalTests = totalTests + 1
+            local test = functionTests[testIndex]
+            local status, result = self:runTest(test)
+            if status == 0 then
+                testsPassed = testsPassed + 1
+            elseif status == 1 then
+                testsFailed = testsFailed + 1
+                allPassed = false
+            else
+                unhandledExceptions = unhandledExceptions + 1
+                allPassed = false
+            end
+            if i < nord then
+                buffer:AppendNL(result)
+            else
+                buffer:Append(result)
+            end
+        end
+        
+        local functionTestsOutput
+        if allPassed then
+            functionTestsOutput = "[✔] All tests passed - "
+                .. functionIndexName
+        else
+            functionTestsOutput = "[❌] Tests failed - "
+                .. functionIndexName
+        end
+        
+        buffer:PrependNL(functionTestsOutput)
+        buffer:Flush()
+    end
+    
+    local resultString = getResultString(
+        totalTests, testsPassed, testsFailed, unhandledExceptions
+    )
+    
+    buffer:AppendNL()
+    buffer:AppendNL(functionGroup._DisplayName)
+    buffer:AppendNL(resultString)
+    buffer:Flush()
+    
+    return totalTests, testsPassed, testsFailed, unhandledExceptions
 end
 
 function Tester:ExecuteTests()
-	local totalTests = 0
-	local testsPassed = 0
-	local testsFailed = 0
-	local unhandledExceptions = 0
-	
-	local testGroupOrder = self.testModule._order
-	
-	for _, groupName in pairs(testGroupOrder) do
-		local testGroupTable = self.testModule[groupName]
-		local groupDisplayName = testGroupTable._DisplayName
-		local stars = string.rep("*", 15)
-		print(stars .. " ".. groupDisplayName .. " " .. stars)
-		
-		local fnTests, fnPassed, fnFailed, fnUnhandled = 
-			self:testFunctionGroup(testGroupTable)
-		
-		totalTests += fnTests
-		testsPassed += fnPassed
-		testsFailed += fnFailed
-		unhandledExceptions += fnUnhandled
-	end
-	
-	local finalResultString = getResultString(
-		totalTests, testsPassed, testsFailed, unhandledExceptions
-	)
-	
-	print("Final results:")
-	print(finalResultString)
+    local totalTests = 0
+    local testsPassed = 0
+    local testsFailed = 0
+    local unhandledExceptions = 0
+    
+    local testGroupOrder = self.testModule._order
+    
+    for _, groupName in pairs(testGroupOrder) do
+        local testGroupTable = self.testModule[groupName]
+        local groupDisplayName = testGroupTable._DisplayName
+        local stars = string.rep("*", 15)
+        print(stars .. " ".. groupDisplayName .. " " .. stars)
+        
+        local fnTests, fnPassed, fnFailed, fnUnhandled = 
+            self:testFunctionGroup(testGroupTable)
+        
+        totalTests = totalTests + fnTests
+        testsPassed = testsPassed + fnPassed
+        testsFailed = testsFailed + fnFailed
+        unhandledExceptions = unhandledExceptions + fnUnhandled
+    end
+    
+    local finalResultString = getResultString(
+        totalTests, testsPassed, testsFailed, unhandledExceptions
+    )
+    
+    print("Final results:")
+    print(finalResultString)
 end
 
 return Tester
