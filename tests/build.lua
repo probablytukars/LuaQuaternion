63972586f2e7a2cb475e8e7a094090ec39e81e91
tukars <psyfw1@nottingham.ac.uk>
2023-08-18 03:19:40 +0100

    prepare quaternion for building

--- tests/build.lua
+++ tests/build.lua
@@ -1,6 +1,51 @@
-package.path = ";.\\?.lua;..\\mocks\\?.lua"
-print(package.path)
-local vector3 = require("Vector3")
+--binaries/windows/lua.exe tests/build.lua
 
-local vec = vector3.new(5, 6, 7)
-print(vec)
+-- http://lua-users.org/wiki/FileInputOutput
+
+local fileName = "src/Quaternion.luau"
+local file = io.open(fileName, "r")
+
+if file then
+    -- Read the contents of the file
+    local content = file:read("*a")  -- "*a" reads the whole file
+
+    -- Close the file
+    file:close()
+
+    -- Do something with the content
+    
+else
+    print("Error opening the file.")
+end
+
+--[==[
+-- see if the file exists
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+-- get all lines from a file, returns an empty 
+-- list/table if the file does not exist
+function lines_from(file)
+    if not file_exists(file) then 
+        print("file does not exist")
+        return {} 
+    end
+    local lines = {}
+    for line in io.lines(file) do 
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+-- tests the functions above
+
+local lines = lines_from(file)
+
+-- print all line numbers and their contents
+for k,v in pairs(lines) do
+    print('line[' .. k .. ']', v)
+end
+--]==]
